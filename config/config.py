48602e8d85ab92d3c73159d8c49db8bd6fc2ccbf
Mate Scharnitzky <mate_scharnitzky@mckinsey.com>
2020-03-09 15:12:08 +0100

    Initial commit

---
+++ config/config.py
@@ -0,0 +1,17 @@
+
+# paths
+
+iris = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+haberman = "https://archive.ics.uci.edu/ml/machine-learning-databases/haberman/haberman.data"
+car = "https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
+titanic = "./data/titanic/train.csv"
+breast_cancer_wisconsin = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+pima_indians_diabetes = "./data/pima-indians-diabetes-database/diabetes.csv"
+give_me_some_credit = "./givemesomecredit/cs-training.csv"
+poker_hand = "https://archive.ics.uci.edu/ml/machine-learning-databases/poker/poker-hand-testing.data"
+flight_delays = "./data/airlinedelaycauses/DelayedFlights.csv"
+hr = "./data/human-resources-data-set/HRDataset_v13.csv"
+german_credit = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
+connect = "https://archive.ics.uci.edu/ml/machine-learning-databases/connect-4/connect-4.data.Z"
+image_segmentation = "https://archive.ics.uci.edu/ml/machine-learning-databases/image/segmentation.data"
+covertype = "https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz"
